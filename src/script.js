92642c0a8e4ca8ce40f84619ea7170670a1d1c0a
Matthew Dylan <dylannavarro65@gmail.com>
2022-04-29 18:36:40 -0400

    created display for three hour forecast

--- src/script.js
+++ src/script.js
@@ -1,14 +1,25 @@
+import './styles.css';
+
 const apiKey = '6badb5bc9855c2296726ae1b1d9be1c0';
 const form = document.querySelector('form');
 
 async function getData(location) {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`, { mode: 'cors' });
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`, { mode: 'cors' });
   const data = await response.json();
-  console.log(data);
+  return data;
 }
-
+async function displayThreeHourForecast(location) {
+  const container = document.getElementById('interval-forecast-container');
+  const data = await getData(location);
+  for (let i = 0; i < 8; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('forecast-container');
+    div.innerHTML = `<div>Date and Time: ${data.list[i].dt_txt}</div><div>Current Temperature: ${data.list[i].main.temp}</div>`;
+    container.append(div);
+  }
+}
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = document.querySelector('input').value;
-  getData(location);
+  displayThreeHourForecast(location);
 });
